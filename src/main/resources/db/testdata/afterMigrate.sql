66fee6b9b4819501151dd057e8f83e1f6c9a9624
jhonnyscerni <jhonnyscerni@gmail.com>
2023-06-29 23:22:51 -0300

    MERC-19 - Implementacao GET Edital

--- src/main/resources/db/testdata/afterMigrate.sql
+++ src/main/resources/db/testdata/afterMigrate.sql
@@ -1,3 +1,4 @@
+delete from public.edital;
 delete from public.endereco;
 delete from public.tb_usuarios_grupos;
 delete from public.tb_grupos;
@@ -31,4 +32,9 @@ VALUES(1, 'marca-teste', NULL);
 INSERT INTO public.produto
 (id, alerta_qtde_estoque, altura, ativo, descricao, largura, link_youtube, nome, peso, profundidade, qtd_estoque, qtde_alerta_estoque, qtde_clique, tipo_unidade, valor_venda, categoria_produto_id, usuario_id, marca_produto_id)
 VALUES(1, true, 0.3, true, 'Produto descrico', 0.4, NULL, 'Nome do Produto', 100.0, 50.0, 10, NULL, NULL, 'cm', 10.00, 1, 1, 1);
+
+INSERT INTO public.edital
+(id, data_fim, data_inicio, numero, usuario_id, endereco_id, produto_id)
+VALUES(1, NULL, NULL, 1, 1, 1, 1);
+
 
