4aca4627d580a1ed8d78563159a48dcc8ed7ccbe
jhonnyscerni <jhonnyscerni@gmail.com>
2023-07-01 01:40:09 -0300

    MERC-22 - Ajustes de Migration e inclusao de usuarios novos

--- src/main/resources/db/testdata/afterMigrate.sql
+++ src/main/resources/db/testdata/afterMigrate.sql
@@ -11,12 +11,26 @@ delete from public.marca_produto;
 INSERT INTO public.tb_usuarios (id,categoria,cnpj,email,insc_estadual,insc_municipal,nome_fantasia,password,razao_social,status_usuario,telefone,tipo_usuario,username) VALUES
 	 (1,'categoria','00120310230123','jhonnyscerni@gmail.com','1234566','123456','SCERNIS','$2y$12$NSsM4gEOR7MKogflKR7GMeYugkttjNhAJMvFdHrBLaLp2HzlggP5W','SCERNI TESTE','ACTIVE','123456','ADMIN','jhonnyscerni');
 
+INSERT INTO public.tb_usuarios (id,categoria,cnpj,email,insc_estadual,insc_municipal,nome_fantasia,password,razao_social,status_usuario,telefone,tipo_usuario,username) VALUES
+	 (2,'categoria','00120310230123','vendedor@gmail.com','1234566','123456','SCERNI-VENDEDOR','$2y$12$NSsM4gEOR7MKogflKR7GMeYugkttjNhAJMvFdHrBLaLp2HzlggP5W','SCERNI VENDEDOR','ACTIVE','123456','ADMIN','vendedor');
+
+INSERT INTO public.tb_usuarios (id,categoria,cnpj,email,insc_estadual,insc_municipal,nome_fantasia,password,razao_social,status_usuario,telefone,tipo_usuario,username) VALUES
+	 (3,'categoria','00120310230123','comprador@gmail.com','1234566','123456','SCERNI-COMPRADOR','$2y$12$NSsM4gEOR7MKogflKR7GMeYugkttjNhAJMvFdHrBLaLp2HzlggP5W','SCERNI COMPRADOR','ACTIVE','123456','ADMIN','comprador');
+
 
 INSERT INTO public.tb_grupos (id,nome) VALUES
 	 (1,'ROLE_ADMIN');
+INSERT INTO public.tb_grupos (id,nome) VALUES
+	 (2,'ROLE_VENDOR');
+INSERT INTO public.tb_grupos (id,nome) VALUES
+	 (3,'ROLE_BUYER');
 
 INSERT INTO public.tb_usuarios_grupos (usuario_id,grupo_id) VALUES
 	 (1,1);
+INSERT INTO public.tb_usuarios_grupos (usuario_id,grupo_id) VALUES
+	 (2,2);
+INSERT INTO public.tb_usuarios_grupos (usuario_id,grupo_id) VALUES
+	 (3,3);
 
 INSERT INTO public.endereco (id,bairro,cep,cidade,complemento,estado,numero,rua_logra,tipo_endereco,uf,usuario_id) VALUES
 	 (1,'Marambaia','66615005','Belem','porto de sines','PA','1495','Av Marambaia','COBRANCA','PA',1);
