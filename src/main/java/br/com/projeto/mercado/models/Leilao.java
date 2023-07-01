c530abb21baade05689731744b4f265459bc21b5
jhonnyscerni <jhonnyscerni@gmail.com>
2023-07-01 19:16:26 -0300

    MERC-24 - Criação da Entidade de Leilao e criação da migration de flyway

---
+++ src/main/java/br/com/projeto/mercado/models/Leilao.java
@@ -0,0 +1,38 @@
+package br.com.projeto.mercado.models;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "leilao")
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@SequenceGenerator(name = "seq_leilao", sequenceName = "seq_leilao", allocationSize = 1, initialValue = 1)
+public class Leilao {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_leilao")
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    private BigDecimal valorLance;
+
+    @ManyToOne(targetEntity = Usuario.class)
+    @JoinColumn(name = "usuario_id", nullable = false,
+            foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "usuario_id_fk"))
+    private Usuario empresa;
+
+
+    @ManyToOne(targetEntity = Edital.class)
+    @JoinColumn(name = "edital_id", nullable = false,
+            foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "edital_id_fk"))
+    private Edital edital;
+
+}
