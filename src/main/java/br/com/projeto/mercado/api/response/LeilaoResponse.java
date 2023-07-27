cef6aede2692b78609a5dcd01e045040385ff2ad
jhonnyscerni <jhonnyscerni@gmail.com>
2023-07-27 16:12:55 -0300

    MERC-B020 -Ajuste do Payload

--- src/main/java/br/com/projeto/mercado/api/response/LeilaoResponse.java
+++ src/main/java/br/com/projeto/mercado/api/response/LeilaoResponse.java
@@ -2,10 +2,14 @@ package br.com.projeto.mercado.api.response;
 
 import lombok.Data;
 
+import java.math.BigDecimal;
+
 @Data
 public class LeilaoResponse {
 
     private Long id;
+
+    private BigDecimal valorLance;
 
     private UsuarioResponse usuario;
 
